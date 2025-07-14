adaa096eca39f94abcf7be7819caca9509a1bd5f
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2025-07-14 10:31:32 +0300

    feat: export PostMessageCallback

--- packages/lib/src/post-messages/index.ts
+++ packages/lib/src/post-messages/index.ts
@@ -2,6 +2,7 @@ export type * from "./types/PostMessages"
 export type { MessageRegistry } from "./types/MessageRegistry"
 export type { PostMessageController } from "./types/PostMessageController"
 export type { PostMessageControllerOptions } from "./types/PostMessageControllerOptions"
+export type { PostMessageCallback } from "./types/PostMessageCallback"
 
 export { createPostMessageController } from "./utils/createPostMessageController"
 export { postMessageTo } from "./utils/postMessageTo"
