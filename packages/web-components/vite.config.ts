f83d2712ca0435742975f0c63229bb4d3aca0386
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2025-07-30 13:13:07 +0300

    fix: process.env in dist

--- packages/web-components/vite.config.ts
+++ packages/web-components/vite.config.ts
@@ -6,52 +6,57 @@ import postcssPlugin from "./postcssPlugin"
 // import tsconfigPaths from "vite-tsconfig-paths"
 import path from "node:path"
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-    dts({
-      tsconfigPath: "tsconfig.app.json",
-      bundleTypes: true
-    }),
-    // tsconfigPaths({
-    //   configNames: [
-    //     "tsconfig.app.json"
-    //   ]
-    // })
-  ],
-  css: {
-    postcss: {
-      plugins: [
-        postcssPlugin()
-      ],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+      tailwindcss(),
+      dts({
+        tsconfigPath: "tsconfig.app.json",
+        bundleTypes: true
+      }),
+      // tsconfigPaths({
+      //   configNames: [
+      //     "tsconfig.app.json"
+      //   ]
+      // })
+    ],
+    css: {
+      postcss: {
+        plugins: [
+          postcssPlugin()
+        ],
+      },
     },
-  },
-  build: {
-    // outDir: "./dist",
-    lib: {
-      entry: "./src/index.ts",
-      formats: [
-        "es",
-      ],
-      fileName: "index",
+    build: {
+      // outDir: "./dist",
+      lib: {
+        entry: "./src/index.ts",
+        formats: [
+          "es",
+        ],
+        fileName: "index",
+      },
+      // rollupOptions: {
+      //   // make sure to externalize deps that shouldn't be bundled
+      //   // into your library
+      //   external: ['vue'],
+      //   output: {
+      //     // Provide global variables to use in the UMD build
+      //     // for externalized deps
+      //     globals: {
+      //       vue: 'Vue',
+      //     },
+      //   },
+      // },
     },
-    // rollupOptions: {
-    //   // make sure to externalize deps that shouldn't be bundled
-    //   // into your library
-    //   external: ['vue'],
-    //   output: {
-    //     // Provide global variables to use in the UMD build
-    //     // for externalized deps
-    //     globals: {
-    //       vue: 'Vue',
-    //     },
-    //   },
-    // },
-  },
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(mode),
     },
   }
 })
